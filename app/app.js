9ffa491e053c55673137c95064f91d5ab75ab1f4
Andres <andres.silva.09@hotmail.com>
2024-01-22 07:13:52 -0300

    se resuelve redireccionamientos desde home , las rutas deben ser relativas a la carpeta public ya que se esta usando express.static('public')

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -18,11 +18,45 @@ app.get('/contacto', (req, res) => {
 });
 
 app.get('/productos', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/products.html'));
+    res.sendFile(path.resolve(__dirname, './views/catalogo.html'));
 });
 
 app.get('/nosotros', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/about.html'));
+});
+
+app.use(express.static('public'));*/
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.listen(8000, () => {
+    console.log('Servidor corriendo en el puerto 8000');
+});
+
+// Define la ruta del directorio de vistas
+const viewsPath = path.join(__dirname, 'views');
+
+// Configura las rutas
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/catalogo', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'catalogo.html'));
+
+});
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'producto.html'));
+
+});
+
+app.get('/nosotros', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'about.html'));
 });
 
 app.use(express.static('public'));
